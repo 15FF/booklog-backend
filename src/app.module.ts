62b1f7bef458f211e8f82838ec94e168060c61c7
qilip <qilip@qilip.io>
2022-06-21 16:33:51 +0900

    Add books module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeORMConfig)
+    TypeOrmModule.forRoot(typeORMConfig),
+    BooksModule
   ],
 })
 export class AppModule {}
