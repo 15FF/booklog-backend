342c13073ff1e7714fd3e391453d07e0bc66b708
greeng00se <thegreengoose7@gmail.com>
2022-06-28 16:24:26 +0900

    refactor: BaseTimeEntity 변경
    - 접근자 추가
    
    Co-authored-by: qilip <qilip@qilip.io>

--- src/book/dto/GetBooksQueryDto.ts
+++ src/book/dto/GetBooksQueryDto.ts
@@ -1,6 +1,5 @@
 import { IsNotEmpty } from "class-validator";
 
-
 export class GetBooksQueryDto {
   @IsNotEmpty()
   bookQuery: string;
--- src/db/time/BaseTimeEntity.ts
+++ src/db/time/BaseTimeEntity.ts
@@ -1,12 +1,28 @@
+import { LocalDateTime } from '@js-joda/core';
 import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
-  
+import { DateTimeUtil } from './util/DateTimeUtil';
+
 export abstract class BaseTimeEntity {
 	@PrimaryGeneratedColumn()
-  	id: number;
+	id: number;
 
-	@CreateDateColumn({ type: 'timestamp' })
+	@CreateDateColumn({
+		type: 'timestamp',
+		nullable: false,
+	})
 	createdAt: Date;
 
-	@UpdateDateColumn({ type: 'timestamp' })
+	@UpdateDateColumn({
+		type: 'timestamp',
+		nullable: false,
+	})
 	updatedAt: Date;
+
+	getCreatedAt(): LocalDateTime {
+		return DateTimeUtil.toLocalDateTime(this.createdAt);
+	}
+
+	getUpdatedAtAt(): LocalDateTime {
+		return DateTimeUtil.toLocalDateTime(this.updatedAt);
+	}
 }
