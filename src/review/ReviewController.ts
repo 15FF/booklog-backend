3d9a93b328c2a19f88eb457026d9f05c08213148
greeng00se <thegreengoose7@gmail.com>
2022-07-12 15:59:46 +0900

    [#32] refactor: validation pipe 전역설정

--- src/review/ReviewController.ts
+++ src/review/ReviewController.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { GetUser } from 'src/auth/GetUserDecorator';
@@ -17,9 +17,7 @@ export class ReviewController {
   @ApiBearerAuth()
   @ApiCreatedResponse({ description: '독서록 등록' })
   @UseGuards(AuthGuard())
-  save(@Body(new ValidationPipe({
-    transform: true
-  })) reviewSaveRequestDto: ReviewSaveRequestDto, @GetUser() user: User): Promise<number> {
+  save(@Body() reviewSaveRequestDto: ReviewSaveRequestDto, @GetUser() user: User): Promise<number> {
     const bookSaveDto: BookSaveDto = reviewSaveRequestDto.toBookSaveDto();
     const reviewSaveDto: ReviewSaveDto = reviewSaveRequestDto.toReviewSaveDto(user);
     return this.reviewService.saveReview(bookSaveDto, reviewSaveDto);
