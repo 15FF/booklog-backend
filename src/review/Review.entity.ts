9c36b5d3daf3ea8b6e1b268cd686d85a93078b1e
greeng00se <thegreengoose7@gmail.com>
2022-07-12 15:47:45 +0900

    [#24] feat: User와 Review 연관관계 설정

--- src/review/Review.entity.ts
+++ src/review/Review.entity.ts
@@ -1,3 +1,4 @@
+import { User } from "src/auth/User.entity";
 import { Book } from "src/book/Book.entity";
 import { BaseTimeEntity } from "src/db/time/BaseTimeEntity";
 import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
@@ -7,8 +8,9 @@ export class Review extends BaseTimeEntity {
   @Column()
   title: string;
 
-  @Column()
-  user: string;
+  @ManyToOne(type => User)
+  @JoinColumn()
+  user: User;
 
   @ManyToOne(type => Book)
   @JoinColumn()
@@ -23,7 +25,7 @@ export class Review extends BaseTimeEntity {
   @Column()
   description: string;
 
-  static from(title: string, user: string, book: Book, rating: number, status: boolean, description: string): Review {
+  static from(title: string, user: User, book: Book, rating: number, status: boolean, description: string): Review {
     const review = new Review()
     review.title = title;
     review.user = user;
