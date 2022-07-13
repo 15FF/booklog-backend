403f708aeabd12a93edce3c1e20e903427a239c1
greeng00se <thegreengoose7@gmail.com>
2022-07-13 16:10:11 +0900

    Revert "[#33] feat: @nestjs/config 추가"
    This reverts commit 7579c76604744bc81baaa896a5bcc0635b5d1c35.

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/AuthModule';
 import { BookModule } from './book/BookModule';
@@ -9,10 +8,6 @@ import { ReviewModule } from './review/ReviewModule';
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env'
-    }),
     BookModule,
     ReviewModule,
     AuthModule
