00372a4e6ab0b63c1a188b0abfe0e9dd29ed4c0d
greeng00se <thegreengoose7@gmail.com>
2022-07-14 09:58:37 +0900

    [#24] test: 독서록 저장 테스트 정리

--- test/review/integration/ReviewController.int.spec.ts
+++ test/review/integration/ReviewController.int.spec.ts
@@ -2,6 +2,7 @@ import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import 'dotenv/config';
+import { User } from "src/auth/User.entity";
 import { UserRepository } from "src/auth/UserRepository";
 import { typeORMConfig } from "src/config/TypeOrmConfig";
 import { ReviewSaveRequestDto } from "src/review/dto/reviewSaveRequestDto";
@@ -9,11 +10,12 @@ import { ReviewController } from "src/review/ReviewController";
 import { ReviewModule } from "src/review/ReviewModule";
 import { ReviewRepository } from "src/review/ReviewRepository";
 
-describe('[BookController]', () => {
+describe('[ReviewController]', () => {
   let app: INestApplication;
   let reviewRepository: ReviewRepository;
-  let reviewController: ReviewController;
   let userRepository: UserRepository;
+  let sut: ReviewController;
+  let user: User;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -24,53 +26,55 @@ describe('[BookController]', () => {
     }).compile();
 
     reviewRepository = moduleRef.get<ReviewRepository>(ReviewRepository);
-    reviewController = moduleRef.get<ReviewController>(ReviewController);
     userRepository = moduleRef.get<UserRepository>(UserRepository);
+    sut = moduleRef.get<ReviewController>(ReviewController);
     
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({
       transform: true
     }));
     await app.init();
+    
+    // 테스트를 위한 사용자 추가
+    const username = 'username';
+    const password = 'password';
+    await userRepository.createUser({ username, password });
+    user = await userRepository.findOneBy({ username: username });
   });
 
   afterEach(async () => {
     await reviewRepository.delete({});
-    await userRepository.delete({});
   });
 
   afterAll(async () => {
+    await userRepository.delete({});
     await app.close();
   });
 
   it('독서록 저장', async () => {
     // given
-    const user = {
-      'username': 'username',
-      'password': 'password',
-    }
-    await userRepository.createUser(user);
-    const savedUser = await userRepository.findOneBy({ username: user.username });
-    
-    const reviewSaveRequestDto: ReviewSaveRequestDto = new ReviewSaveRequestDto (
-      "greeng00se 장편선",
-      "https:\/\/fake.example.com\/asdf\/123\/456\/12345678.jpg?type=m1&udate=amavvdko",
-      "qilip",
-      "곤운",
-      "2016-01-01",
-      "9791141844829",
-      "qilip의 greeng00se 사냥기 100.0",
-      9,
-      true,
-      "독서록 내용"
-    );
+    const reviewSaveRequestDto: ReviewSaveRequestDto = dummyReviewSaveRequestDto();
 
     // when
-    await reviewController.save(reviewSaveRequestDto, savedUser);
+    await sut.save(reviewSaveRequestDto, user);
 
     // then
     const count = await reviewRepository.count();
     expect(count).toBe(1);
   });
-  
 });
+
+function dummyReviewSaveRequestDto(): ReviewSaveRequestDto {
+  return new ReviewSaveRequestDto (
+    "greeng00se 장편선",
+    "https:\/\/fake.example.com\/asdf\/123\/456\/12345678.jpg?type=m1&udate=amavvdko",
+    "qilip",
+    "곤운",
+    "2016-01-01",
+    "9791141844829",
+    "qilip의 greeng00se 사냥기 100.0",
+    9,
+    true,
+    "독서록 내용"
+  );
+}
--- test/review/integration/ReviewService.int.spec.ts
+++ test/review/integration/ReviewService.int.spec.ts
@@ -2,6 +2,7 @@ import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import 'dotenv/config';
+import { User } from "src/auth/User.entity";
 import { UserRepository } from "src/auth/UserRepository";
 import { typeORMConfig } from "src/config/TypeOrmConfig";
 import { ReviewSaveRequestDto } from "src/review/dto/reviewSaveRequestDto";
@@ -9,11 +10,12 @@ import { ReviewModule } from "src/review/ReviewModule";
 import { ReviewRepository } from "src/review/ReviewRepository";
 import { ReviewService } from "src/review/ReviewService";
 
-describe('[BookService]', () => {
+describe('[ReviewService]', () => {
   let app: INestApplication;
   let reviewRepository: ReviewRepository;
-  let reviewService: ReviewService;
   let userRepository: UserRepository;
+  let sut: ReviewService;
+  let user: User;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -24,14 +26,20 @@ describe('[BookService]', () => {
     }).compile();
 
     reviewRepository = moduleRef.get<ReviewRepository>(ReviewRepository);
-    reviewService = moduleRef.get<ReviewService>(ReviewService);
     userRepository = moduleRef.get<UserRepository>(UserRepository);
+    sut = moduleRef.get<ReviewService>(ReviewService);
     
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({
       transform: true
     }));
     await app.init();
+
+    // 테스트를 위한 사용자 추가
+    const username = 'username';
+    const password = 'password';
+    await userRepository.createUser({ username, password });
+    user = await userRepository.findOneBy({ username: username });
   });
 
   afterEach(async () => {
@@ -45,30 +53,12 @@ describe('[BookService]', () => {
 
   it('독서록 저장', async () => {
     // given
-    const user = {
-      'username': 'username',
-      'password': 'password',
-    }
-    await userRepository.createUser(user);
-    const savedUser = await userRepository.findOneBy({ username: user.username });
-    
-    const reviewSaveRequestDto: ReviewSaveRequestDto = new ReviewSaveRequestDto (
-      "greeng00se 장편선",
-      "https:\/\/fake.example.com\/asdf\/123\/456\/12345678.jpg?type=m1&udate=amavvdko",
-      "qilip",
-      "곤운",
-      "2016-01-01",
-      "9791141844829",
-      "qilip의 greeng00se 사냥기 100.0",
-      9,
-      true,
-      "독서록 내용"
-    );
+    const reviewSaveRequestDto: ReviewSaveRequestDto = dummyReviewSaveRequestDto();
 
     // when
-    await reviewService.saveReview(
+    await sut.saveReview(
       reviewSaveRequestDto.toBookSaveDto(), 
-      reviewSaveRequestDto.toReviewSaveDto(savedUser)
+      reviewSaveRequestDto.toReviewSaveDto(user)
     );
 
     // then
@@ -76,3 +66,18 @@ describe('[BookService]', () => {
     expect(count).toBe(1);
   });
 });
+
+function dummyReviewSaveRequestDto(): ReviewSaveRequestDto {
+  return new ReviewSaveRequestDto (
+    "greeng00se 장편선",
+    "https:\/\/fake.example.com\/asdf\/123\/456\/12345678.jpg?type=m1&udate=amavvdko",
+    "qilip",
+    "곤운",
+    "2016-01-01",
+    "9791141844829",
+    "qilip의 greeng00se 사냥기 100.0",
+    9,
+    true,
+    "독서록 내용"
+  );
+}
