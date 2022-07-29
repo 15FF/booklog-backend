843af263cd884e3f7b3d47784115d9949967d3c0
qilip <qilip@qilip.io>
2022-07-29 21:47:22 +0900

    chore: 변수 네이밍 통일

--- src/book/BookController.ts
+++ src/book/BookController.ts
@@ -6,11 +6,11 @@ import { GetBooksQueryDto } from './dto/GetBooksQueryDto';
 @ApiTags('Books')
 @Controller('books')
 export class BookController {
-  constructor(private booksService: BookService) {}
+  constructor(private bookService: BookService) {}
   
   @Get()
   @ApiOkResponse({ description: '검색 쿼리에 따른 책 목록 반환' })
   getBooks(@Query() getBooksQueryDto: GetBooksQueryDto) {
-    return this.booksService.getBooks(getBooksQueryDto.bookQuery);
+    return this.bookService.getBooks(getBooksQueryDto.bookQuery);
   }
 }
